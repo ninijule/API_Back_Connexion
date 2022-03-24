dfb015bf90fb9444c7aaa968694b2214ee60a539
Julian <julian.beutin@viacesi.fr>
2022-03-24 17:00:44 +0100

    :sparkles: Add sanitize email + password

--- dist/routes/login.js
+++ dist/routes/login.js
@@ -7,6 +7,6 @@ const express_1 = __importDefault(require("express"));
 const express_validator_1 = require("express-validator");
 const AuthController_1 = __importDefault(require("../controller/AuthController"));
 const router = express_1.default.Router();
-router.post("/", (0, express_validator_1.body)("email").isEmail(), (0, express_validator_1.body)("password").escape().stripLow().isLength({ min: 5 }), AuthController_1.default.login);
+router.post("/", (0, express_validator_1.body)("email").isEmail().isLength({ min: 1 }), (0, express_validator_1.body)("password").escape().stripLow().isLength({ min: 5 }), AuthController_1.default.login);
 exports.default = router;
 //# sourceMappingURL=login.js.map
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -4,6 +4,6 @@ import AuthController from "../controller/AuthController";
 
 const router = express.Router();
 
-router.post("/", body("email").isEmail(), body("password").escape().stripLow().isLength({ min: 5 }), AuthController.login);
+router.post("/", body("email").isEmail().isLength({ min: 1 }), body("password").escape().stripLow().isLength({ min: 7 }), AuthController.login);
 
 export default router;
