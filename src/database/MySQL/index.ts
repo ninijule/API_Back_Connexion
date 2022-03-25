862aa132c006de439ff973eb3bead0f45aae35ea
Julian <julian.beutin@viacesi.fr>
2022-03-25 16:30:28 +0100

    :sparkles: Add dockerfile

--- src/database/MySQL/index.ts
+++ src/database/MySQL/index.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('test', 'root', '', {
+const sequelize = new Sequelize('test', 'root', 'test', {
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306
